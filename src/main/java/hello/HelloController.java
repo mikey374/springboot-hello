986d9b7b9d61d02f5950d9593907bdc9cd785e26
mikey374 <vardhanmanu00@gmail.com>
2024-11-07 12:55:16 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -21,7 +21,7 @@ public class HelloController {
         }
 
         // Return the greeting message with the IPv4 address
-        return "Greetings from  'pavani' deployed JAVA app in elastic bean stalk..!!! Host private IPv4 Address: " + ipAddress;
+        return "Greetings from  'arun' deployed JAVA app in elastic bean stalk..!!! Host private IPv4 Address: " + ipAddress;
     }
     
 }
